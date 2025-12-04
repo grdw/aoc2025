bf0f69ba19f5f5475659c272781f36824f3421ed
grdw <gerardwesterhof@vandebron.nl>
2025-12-04 09:55:50 +0100

    day4p2 is free

--- p04/src/main.rs
+++ p04/src/main.rs
@@ -16,7 +16,7 @@ fn main() -> Result<(), io::Error> {
         .collect::<PaperRolls>();
 
     println!("p1: {}", part1(&paper_rolls));
-    //println!("p2: {}", part2(&paper_rolls));
+    println!("p2: {}", part2(&paper_rolls));
 
     Ok(())
 }
@@ -43,7 +43,6 @@ fn part1(paper_rolls: &PaperRolls) -> u16 {
                 if c < 4 {
                     total += 1;
                 }
-                // check if there are fewer than 4
             }
         }
     }
@@ -67,54 +66,40 @@ fn count_surrounding_rolls(paper_rolls: &PaperRolls, x: isize, y: isize) -> u16 
     return count
 }
 
-//fn part2(paper_rolls: &PaperRolls) -> u64 {
-//    let mut counter = 0;
-//    let powered_batteries: usize = 12;
-//
-//    for bank in paper_rolls {
-//        let mut joltage: u64 = 0;
-//        let mut idx = 0;
-//        let mut picks = 0;
-//
-//        loop {
-//            let midx = cmp::min(idx+(bank.len()+1-powered_batteries), bank.len());
-//            let mut max = 0;
-//            for j in idx..midx {
-//                if bank[j] > max {
-//                    max = bank[j];
-//                    idx = j;
-//                }
-//            }
-//
-//            joltage = joltage*10 + (bank[idx] as u64);
-//            idx += 1;
-//            picks += 1;
-//
-//            println!("{:?} {} {}", joltage, picks, midx);
-//            if picks == powered_batteries {
-//                break;
-//            }
-//
-//            if idx == bank.len() {
-//                break;
-//            }
-//        }
-//        counter += joltage;
-//    }
-//    return counter
-//}
-//
-//#[test]
-//fn test_part2() {
-//    assert_eq!(
-//        part2(
-//            &vec![
-//                vec![9,8,7,6,5,4,3,2,1,1,1,1,1,1,1],
-//                vec![8,1,1,1,1,1,1,1,1,1,1,1,1,1,9],
-//                vec![2,3,4,2,3,4,2,3,4,2,3,4,2,7,8],
-//                vec![8,1,8,1,8,1,9,1,1,1,1,2,1,1,1]
-//            ]
-//        ),
-//        3121910778619
-//    );
-//}
+fn part2(paper_rolls: &PaperRolls) -> u64 {
+    let mut total = 0;
+    let mut altered_rolls = paper_rolls.clone();
+
+    loop {
+        let removed = remove_rolls(&mut altered_rolls);
+
+        if removed == 0 {
+            break;
+        }
+
+        total += removed;
+    }
+    return total
+}
+
+fn remove_rolls(paper_rolls: &mut PaperRolls) -> u64 {
+    let mut total = 0;
+    let row_len = paper_rolls[0].len();
+
+    for y in 0..paper_rolls.len() {
+        for x in 0..row_len {
+            if paper_rolls[y][x] == '@' {
+                let c = count_surrounding_rolls(
+                    paper_rolls,
+                    x as isize,
+                    y as isize
+                );
+                if c < 4 {
+                    paper_rolls[y][x] = '.';
+                    total += 1;
+                }
+            }
+        }
+    }
+    return total
+}
